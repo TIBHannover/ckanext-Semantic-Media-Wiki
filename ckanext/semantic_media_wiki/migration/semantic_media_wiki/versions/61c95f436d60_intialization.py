b0510a2546edeb851a1c098f4df8294bc4b03df2
pooya.oa <pooya.oladazimi@tib.eu>
2021-10-22 13:32:25 +0200

    added migration

---
+++ ckanext/semantic_media_wiki/migration/semantic_media_wiki/versions/61c95f436d60_intialization.py
@@ -0,0 +1,32 @@
+"""intialization
+
+Revision ID: 61c95f436d60
+Revises: 
+Create Date: 2021-10-22 13:30:05.495509
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '61c95f436d60'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+     op.create_table(
+        'resource_mediawiki_link',
+        sa.Column('id', sa.Integer, primary_key=True, nullable=False),
+        sa.Column('resource_id', sa.UnicodeText(), sa.ForeignKey('resource_id'), nullable=False),
+        sa.Column('url', sa.UnicodeText(), nullable=False),
+        sa.Column('link_name', sa.UnicodeText()),
+        sa.Column('create_at', sa.DateTime(timezone=False), nullable=False),
+        sa.Column('updated_at', sa.DateTime(timezone=False), nullable=False),
+    )
+
+
+def downgrade():
+   op.drop_table('resource_mediawiki_link')
