8f38d587c9adf819b524bbda93c3c36b431b87fb
pooya.oa <pooya.oladazimi@tib.eu>
2023-02-09 18:02:13 +0100

    cleaning

--- ckanext/semantic_media_wiki/libs/sample_link.py
+++ ckanext/semantic_media_wiki/libs/sample_link.py
@@ -45,7 +45,7 @@ class SampleLinkHelper():
         '''
         
         try:
-            for i in range(1, resources_len + 30):    
+            for i in range(1, resources_len):    
                 link = request.form.get('sample_link' + str(i))
                 if link == '0': # not specified
                     continue            
