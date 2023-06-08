44ea9d83dfaa4b63fc850af21709f4ede8fb4817
pooya.oa <pooya.oladazimi@tib.eu>
2023-06-08 13:49:26 +0200

    implemented save protocol

--- ckanext/semantic_media_wiki/controllers/protocol_link.py
+++ ckanext/semantic_media_wiki/controllers/protocol_link.py
@@ -1,8 +1,38 @@
 # encoding: utf-8
 
+from flask import request, redirect
+from datetime import datetime as _time
+import ckan.lib.helpers as h
+from ckanext.semantic_media_wiki.models.dataset_protocol_link import DatasetProtocolLink
+
+
 
 class ProtocolLinkController():
 
     @staticmethod
     def index():
         return "0"
+
+
+    @staticmethod
+    def save_protocol_link():
+        try:
+            dataset_id = request.form.get('dataset_id')
+            protocol_url = request.form.get('protocol_url')
+            protocol_name = request.form.get('protocol_name')
+            created_at = _time.now()
+            updated_at = create_at
+            db_model = DatasetProtocolLink(
+                    dataset_id==dataset_id, 
+                    protocol_url==protocol_url, 
+                    protocol_name=protocol_name, 
+                    created_at=created_at, 
+                    updated_at=updated_at
+            )
+            db_model.save()
+            return redirect(h.url_for('dataset.read', id=str(dataset_id) ,  _external=True))
+
+        except:
+            return toolkit.abort(500, "Server issue")
+
+    
